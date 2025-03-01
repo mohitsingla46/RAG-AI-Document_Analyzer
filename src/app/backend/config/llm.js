8f6ea0d4fc13b3799a4bcd2703ea3ee90e1f91cf
Mohit Singla <mhtkumar46@gmail.com>
2025-03-01 20:31:44 +0530

    llm changed

--- src/app/backend/config/llm.js
+++ src/app/backend/config/llm.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { ChatGroq } from "@langchain/groq";
 
 export const llm = new ChatGroq({
-    model: "llama3-70b-8192",
+    model: "llama-3.3-70b-versatile",
     temperature: 0,
     maxRetries: 2,
 });
